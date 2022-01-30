65df653b1f798daae9c79671046e0755ef86aa4c
preetamyadav <preetam.hbti@gmail.com>
2022-01-31 00:04:06 +0530

    Time: 1 ms (58.24%), Space: 44.1 MB (6.55%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start =0;
+        int end = nums.length -1;
+        
+        while(start <= end){
+            if(nums[start] == target || nums[end] == target){
+                return true;
+            }
+            start++;
+            end --;
+        }
+        return false;
+        
+    }
+}
